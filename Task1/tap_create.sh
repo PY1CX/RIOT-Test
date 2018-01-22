9dca185489a70f99059ef2430c32fa38309b131d
Felipe Navarro <felnavarro2@gmail.com>
2018-01-22 00:28:17 -0200

    Add files via upload

---
+++ Task1/tap_create.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#
+# Script for creating the TAP devices necessary to test our code
+# Need to chmod +x tap_create.sh to become an executable
+#
+
+../../dist/tools/tapsetup/tapsetup -c
