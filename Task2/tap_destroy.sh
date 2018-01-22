c1abd4af288284a555b81ed974d16f40a3bc8457
Felipe Navarro <felnavarro2@gmail.com>
2018-01-22 03:19:35 -0200

    Task 2 commit

---
+++ Task2/tap_destroy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#
+# Script for deleting the TAP devices necessary to test our code
+# Need to chmod +x tap_destroy.sh to become an executable
+#
+
+../../dist/tools/tapsetup/tapsetup -d
